0f923ef8aff82a52e58940e436c4b72e5b11d09b
Thevenel <47800812+Thevenel@users.noreply.github.com>
2020-11-30 21:39:08 -0400

    Updated user profile

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.forms import fields
 from django.forms.fields import EmailField
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +10,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2' ]
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ['image']
